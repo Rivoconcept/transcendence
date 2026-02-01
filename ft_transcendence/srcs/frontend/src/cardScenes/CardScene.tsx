4ac473741b9f32fdc32dd3c8ddaba6e9fffd11d0
Rivo Vonjiniaina Hanitrarivelo <rhanitra@c1r11s9.42antananarivo.mg>
2026-02-01 15:39:22 +0300

    no space

--- ft_transcendence/srcs/frontend/src/cardScenes/CardScene.tsx
+++ ft_transcendence/srcs/frontend/src/cardScenes/CardScene.tsx
@@ -63,53 +63,64 @@ export default function CardScene() {
             </div>
           ))}
 
-          <div className="dashboard">
-            <div className="dashboardTop">
-              <div className="avatar">
-                <img src="/avatar.png" alt="avatar" />
-              </div>
-              <div className="circleTimer">
-                <ProgressCircleTimer />
-              </div>
-            </div>
-            <hr className="separator"/>
-            {/* PROGRESS BAR */}
-            <div className="progressBarScore">
-              <div className="progressTile">
-                <span className="label">PROGRESS</span>
-                <span className="line" />
-                <span className="turn">{turn} / 5</span>
-              </div>
-              <ProgressBar />
-            </div>
+            <div className="dashboard">
+              <div className="card-group">
+                <div className="card border-0 bg-black text-light">
+                  <div className="card-body">
+                    <div className="avatar">
+                      <img src="/avatar.png" alt="avatar" />
+                    </div>
+                  </div>
+                </div>
+                <div className="card border-0 bg-black text-light">
 
-            {/* SCORE HISTORY */}
+                  <div className="card-body">
+                    <div className="circleTimer">
+                      <ProgressCircleTimer />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <hr className="separator"/>
+              {/* PROGRESS BAR */}
+              <div className="progressBarScore">
+                <div className="progressTile">
+                  <span className="label">PROGRESS</span>
+                  <span className="line" />
+                  <span className="turn">{turn} / 5</span>
+                </div>
+                <ProgressBar />
+              </div>
+              <div className="card-group">
+                <div className="card border-0 bg-black text-light">
+                  <div className="card-body">
+                    <ul className="scoreLists">
+                      {scores.map((s, i) => (
+                        <ScoreList key={i} score={s} round={i + 1} />
+                      ))}
+                    </ul>
+                    <div className="totalScore">
+                      <div className="separatorLine" />
+                      <p>Total Score <span>{totalScore}</span></p>
+                    </div>
+                  </div>
+                </div>
+                <div className="card border-0 bg-black text-light">
 
-            <div className="dashboardTop">
-            <div className="scoreHistoryWrapper">
-              <ul className="scoreLists">
-                {scores.map((s, i) => (
-                  <ScoreList key={i} score={s} round={i + 1} />
-                ))}
-              </ul>
-              <div className="totalScore">
-                <div className="separatorLine" />
-                <p>Total Score <span>{totalScore}</span></p>
+                  <div className="card-body">
+                    {isWin && <><h2 className="win">🎉 </h2> <h2 className="win">You Win!</h2></>}
+                    {isLose && !isWin && <><h2 className="lose">💀</h2><h2 className="lose">Perdu</h2></>}
+                  </div>
+                </div>
+              </div>
+              {/* BUTTON */}
+              <div className="separatorBottom">
+                <hr className="separator"/>
+              </div>
+              <div className="cardButton">
+                <PhaseButton phase={phase} onClick={onButtonClick} />
               </div>
             </div>
-
-            <div className="resultStatus">
-              {isWin && <><h1>🎉 </h1> <h2 className="win">Gagné !</h2></>}
-              {isLose && !isWin && <h2 className="lose">💀 Perdu</h2>}
-            </div>
-          </div>
-            {/* TOTAL SCORE */}
-            {/* BOUTON */}
-            <div className="cardButton">
-              <PhaseButton phase={phase} onClick={onButtonClick} />
-            </div>
-
-          </div>
         </div>
       </div>
 
